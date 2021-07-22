b18b3c66154b6b3b5f7d3db76df33db0311f5f40
dkdeconti <deconti.d@gmail.com>
2021-07-21 23:22:22 -0400

    Modified script to look at complement and overhang

--- analysis/src/dinucleotide_loci_from_FASTA.py
+++ analysis/src/dinucleotide_loci_from_FASTA.py
@@ -5,9 +5,46 @@ import re
 import sys
 
 
-def get_dinucleotide(fasta_filename, dinucleotide):
-    """Parses FASTA for dinucleotide loci to print as BED format.
+def find_indices(prev, current, nuc):
+    """Finds matching indices from previous seq plus one less than nuc
+    seq from next line. (Finds overlap between lines.)
     """
+    if current != None:
+        buf = current[0: len(nuc)-1]
+    else:
+        buf = ""
+    return [
+        m.start() 
+        for m in re.finditer(
+            nuc, 
+            prev + buf
+        )
+    ]
+
+
+def print_bed(indices, contig, loci, stepper):
+    for match in indices:
+        out_cols = [
+            contig,
+            str(loci + match),
+            str(loci + match + stepper)
+        ]
+        sys.stdout.write('\t'.join(out_cols) + '\n')
+
+
+def get_dinucleotide(fasta_filename, nuc_seq):
+    """Parses FASTA for dinucleotide loci to print as BED format.
+
+    Note: we delay printing until the next line so that we can
+    add a buffer to the line to account for spanning nucleotide sequence.
+    """
+    nuc_comp_map = {
+        "A" : "T",
+        "T" : "A",
+        "C" : "G",
+        "G" : "C"
+    }
+    comp_nuc_seq = ''.join([nuc_comp_map[c] for c in nuc_seq])
     # Make sure the file exists, otherwise throw an error
     try:
         f = open(fasta_filename, 'r')
@@ -16,13 +53,14 @@ def get_dinucleotide(fasta_filename, dinucleotide):
         sys.stderr.write("FileNotFoundError: %s\n" % str(e))
     current_contig = None
     loci_tracker = 0
+    first_line = True
     with open(fasta_filename, 'r') as handle:
         for line in handle:
             # Checks for first line / first contig
             # In case there's unnecessary header before the contigs
             # (Should not happen, but just in case.)
-            if current_contig == "None" and line[0] == ">":
-                # Strinp the new line, skip the '>' and split 
+            if current_contig == None and line[0] == ">":
+                # Strip the new line, skip the '>' and split 
                 # the line by white space
                 # Contig name is first words before white space
                 current_contig = line.strip('\n')[1:].split(' ')[0]
@@ -30,24 +68,62 @@ def get_dinucleotide(fasta_filename, dinucleotide):
                 loci_tracker = 0
                 continue # continue to next loop
             elif line[0] == ">": # Checks if new contig
+                # print last sequence line from previous contig
+                indices = sorted(
+                    find_indices(
+                        prev_seq, None, nuc_seq
+                    ) + find_indices(
+                        prev_seq, None, comp_nuc_seq
+                    )
+                )
+                print_bed(
+                    indices,
+                    current_contig,
+                    loci_tracker,
+                    len(nuc_seq)
+                )
+                # then reset everything for the new contig
                 current_contig = line.strip('\n')[1:].split(' ')[0]
                 loci_tracker = 0
+                first_line = True
                 continue # continue to next loop
+            elif first_line == True:
+                # if first line of sequence, store and move to next line
+                prev_seq = line.strip('\n')
+                first_line = False
+                continue
             seq = line.strip('\n') # remove the new line character
-            # Find location of all dinucleotides in the single line
-            match_indices = [m.start() for m in re.finditer(dinucleotide, seq)]
-            # Loop through all the found dinucleotides
-            for match in match_indices:
-                # Create the columns of the BED file
-                out_cols = [
-                    current_contig,
-                    str(loci_tracker + match),
-                    str(loci_tracker + match + len(dinucleotide))
-                ]
-                # Print out to columns tab-delimited to the terminal
-                sys.stdout.write('\t'.join(out_cols) + '\n')
+            # Find location of all dinucleotides in the previous line
+            indices = sorted(
+                find_indices(
+                    prev_seq, seq, nuc_seq
+                ) + find_indices(
+                    prev_seq, seq, comp_nuc_seq
+                )
+            )
+            print_bed(
+                indices,
+                current_contig,
+                loci_tracker,
+                len(nuc_seq)
+            )
             # Before moving to the next loop, update loci_tracker
-            loci_tracker += len(seq)
+            loci_tracker += len(prev_seq)
+            prev_seq = seq
+    # Print final line of sequence b/c we delay the seq addition
+    indices = sorted(
+        find_indices(
+            prev_seq, None, nuc_seq
+        ) + find_indices(
+            prev_seq, None, comp_nuc_seq
+        )
+    )
+    print_bed(
+        indices,
+        current_contig,
+        loci_tracker,
+        len(nuc_seq)
+    )
 
 
 
